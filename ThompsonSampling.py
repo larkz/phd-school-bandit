0de231b8c3453727837040e694c3d4e85549e7c1
larkz <liu.larkin@gmail.com>
2022-07-23 12:53:26 +0200

    updates

--- ThompsonSampling.py
+++ ThompsonSampling.py
@@ -6,7 +6,7 @@ from BanditTools import *
 class ThompsonSamplingDiscrete:
     def __init__(self, nbArms, maxDiscrete = None):
         self.nbArms = nbArms
-        self.actionSpace = list(range(0, len(maxDiscrete))) if maxDiscrete is None else list(range(0, len(maxDiscrete)))
+        self.actionSpace = list(range(0, nbArms)) if maxDiscrete is None else list(range(0, len(maxDiscrete)))
         self.clear()
 
     def clear(self):
@@ -17,19 +17,19 @@ class ThompsonSamplingDiscrete:
     
     def selectArm(self):
         if not self.all_arms_played:
-            for arm in self.nbArms:
+            for arm in range(self.nbArms):
                 if self.nbDraws[arm] < 1:
                     return arm
             self.all_arms_played = True
         
-        mu_rewards_est = np.zeros(self.nbArms)
+        mu_rewards_est_array = np.zeros(self.nbArms)
         for arm in range(len(self.actionSpace)):
             # Estimate mean (clipped normal)
             mu_rewards_est = self.cumRewards[arm] / self.nbDraws[arm]
             # Draw sample reward
-            mu_rewards_est[arm] = np.clip(np.random.normal(mu_rewards_est), 0, np.Inf)
+            mu_rewards_est_array[arm] = np.clip(np.random.normal(mu_rewards_est), 0, np.Inf)
         
-        selected_action = np.argmax(mu_rewards_est)
+        selected_action = np.argmax(mu_rewards_est_array)
         return selected_action
 
     def playArm(self, arm, reward):
--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import numpy as np
 from ThompsonSampling import *
 from market import *
+import matplotlib.pyplot as plt
 
+T = 2000
+N_ARMS = 30
 if __name__ == "__main__":
 
-    ts = ThompsonSamplingDiscrete(30)
+    ts = ThompsonSamplingDiscrete(N_ARMS)
     ts.clear()
-    
-    p = ts.select_action()
-    reward_trial = profit_function(p)
-    ts.playArm(p, reward_trial)
 
-    
+    rewards = []
+    prices_played = []
+    for t in range(T):
+        p = ts.selectArm()
+        reward_trial = profit_function(p)
+        ts.playArm(p, reward_trial)
+        print(reward_trial)
+        rewards.append(reward_trial)
+        prices_played.append(p)
+
+    print("optimal price: " +str( -(MAX_DEMAND/ELASTICITY)/2 ))
+
+    plt.xlabel("Time t")
+    plt.ylabel("Epoch Reward")
+    plt.plot(rewards)
+    plt.savefig("reward_policy.png", dpi=300)
+    plt.show()
+
+    plt.xlabel("Time t")
+    plt.ylabel("Prices Played")
+    plt.plot(prices_played)
+    plt.savefig("prices.png", dpi=300)
+    plt.show()
 
